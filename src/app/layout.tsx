d629d40c6345252d8295d464a5c8f8751678f293
TheAstronautGuy <theastronautguy2@gmail.com>
2026-07-15 10:58:39 +0530

    removed tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,12 +32,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T45FD5MQ');</script>
-<!-- End Google Tag Manager -->
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -87,9 +81,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <body
         className={`${jetbrainsMono.variable} ${jetbrainsMono.className} antialiased min-h-screen flex flex-col`}
       >
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T45FD5MQ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
         <GSAPProvider>
           <Header/>
           <main className="flex-1">
