503c70f447597f3c2ff06dd8dd56f4758ef1768b
MrAstronaut <121187011+AstronautGuy@users.noreply.github.com>
2026-07-15 10:44:31 +0530

    Integrate Google Tag Manager into layout
    Added Google Tag Manager scripts for tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T45FD5MQ');</script>
+<!-- End Google Tag Manager -->
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -81,6 +88,10 @@ export default function RootLayout({
       <body
         className={`${jetbrainsMono.variable} ${jetbrainsMono.className} antialiased min-h-screen flex flex-col`}
       >
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T45FD5MQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <GSAPProvider>
           <Header/>
           <main className="flex-1">
